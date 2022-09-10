3a61bfdd1def0694289cacb93d522f7522d6c890
junior barros <juniorbaarros@gmail.com>
2022-09-09 22:07:11 -0300

    created vue app

---
+++ app/src/pages/Puzzle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>PUzzle</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "puzzle-content",
+});
+</script>
+
+<style></style>
