55f3182ddc1e7858cd6b8175d4db364c9893c8ba
Junior Barros <juniorbaarros@gmail.com>
2022-09-17 22:42:51 -0300

    feat: created layout of puzzle-8

--- app/src/pages/History.vue
+++ app/src/pages/History.vue
@@ -1,20 +1,84 @@
 <template>
-  <aside>
-    <h2>Historico</h2>
-  </aside>
+  <div class="historyContainer">
+    <div class="historyBox">
+      <div
+        class="historyCard"
+        v-for="(history, historyIndex) in historyArray"
+        :key="historyIndex"
+      >
+        <div class="passo">Passo {{ historyIndex }}</div>
+        <div class="historyCardItem">
+          <div
+            v-for="historyValue in history"
+            class="historyCardItemValues"
+            :key="historyValue"
+            :class="{ 'puzzle-active': historyValue == 0 }"
+          >
+            {{ historyValue == 0 ? "" : historyValue }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  props: {
+    historyArray: [],
+  },
   name: "HistoryComponent",
 });
 </script>
 
 <style scoped>
-aside {
+.historyContainer {
+  width: 100%;
+  height: 100%;
+  max-height: 520px;
+  max-width: 100%;
+  overflow: auto;
+}
+
+.historyBox {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+}
+
+.historyCard {
+  background: #333333;
+  display: flex;
+  height: 150px;
+}
+.historyCardItem {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  border: 10px solid #232323;
+}
+.passo {
   background: #232323;
+
+  font-size: 1.1rem;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35%;
+  border: 10px solid #333333;
   border-radius: 10px;
+}
+.historyCardItemValues {
+  border-radius: 10px;
+  max-width: 60px;
+  padding: 5px;
+}
+.puzzle-active {
+  background: #157347;
 }
 </style>
--- app/src/pages/Puzzle.vue
+++ app/src/pages/Puzzle.vue
@@ -1,25 +1,37 @@
 <template>
   <main class="row col-12">
-    <section class="col-5 container">
+    <section class="col-6 container">
       <div class="row mb-4">
-        <div class="col-4 mb-2">
-          <label for="algoritimo" class="form-label">Tipo de Busca</label>
-          <select class="form-select" id="algoritimo">
-            <option value="1" selected>A*</option>
+        <div class="col-2">
+          <label for="algoritimo" class="form-label">Busca</label>
+          <select class="form-select" id="algoritimo" v-model="searchMethod">
+            <option value="1">A*</option>
             <option value="2">Largura</option>
           </select>
         </div>
-        <div class="col-5 mb-2">
-          <label for="algoritimo" class="form-label">Ordem</label>
+        <div class="col-2" v-show="isStar">
+          <label for="profundidade" class="form-label">Profundidade</label>
+          <select class="form-select" id="profundidade" v-model="depth">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+        <div class="col-3">
+          <label for="algoritimo" class="form-label">ordem</label>
           <input
+            v-model="userValue"
             type="text"
             class="form-control"
-            id="ordem"
-            placeholder="insira a ordem 0-8 "
+            id="order"
+            :class="{ success: success, error: error }"
+            placeholder="[0-8] ex: 012345678"
           />
         </div>
-        <div>
-          <button class="btn btn-danger w-25 mt-1 mb-1">
+        <div class="col">
+          <button class="btn btn-success mt-1 mb-1 mx-1" @click="setOrder">
+            Embaralhar
+          </button>
+          <button class="btn btn-danger mt-1 mb-1 mx-1" @click="randomize">
             <svg
               width="24"
               height="24"
@@ -33,24 +45,33 @@
             </svg>
           </button>
         </div>
+        <span v-show="error" class="error p-2 mt-2">
+          Para conseguir embaralhar manualmente de forma adequada. É necessario
+          inserir 9 numeros, sendo obrigatorio 1 numero 0 e os restante entre 1
+          a 8
+        </span>
       </div>
 
       <div class="puzzle-temp border-0 border-radius-2">
         <div
-          v-for="i in 9"
+          v-for="i in order"
           :key="i"
           class="puzzle-obj"
-          :class="{ 'puzzle-active': i - 1 == 0 }"
+          :class="{ 'puzzle-active': i == 0 }"
         >
-          {{ i - 1 }}
+          {{ i == 0 ? "" : i }}
         </div>
       </div>
-      <button class="col-12 btn btn-success btn-lg mt-2 mb-2">
+      <button class="col-6 btn btn-success btn-lg mt-2 mb-2" @click="solve">
         Solucionar
       </button>
     </section>
-
-    <history class="col-6" />
+    <div class="col-6 aside">
+      <aside>
+        <h2>Historico</h2>
+      </aside>
+      <history v-if="solved" :historyArray="historyData" />
+    </div>
   </main>
 </template>
 
@@ -61,10 +82,89 @@ import History from "./History.vue";
 export default defineComponent({
   components: { History },
   name: "PuzzleContent",
+  emits: ["change"],
+  data() {
+    return {
+      isStar: 1,
+      searchMethod: "1",
+      depth: 1,
+      order: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      userValue: "012345678",
+      error: false,
+      success: false,
+      historyData: [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        [1, 0, 2, 3, 4, 5, 6, 7, 8],
+        [1, 2, 0, 3, 4, 5, 6, 7, 8],
+        [1, 2, 3, 0, 4, 5, 6, 7, 8],
+        [1, 2, 3, 4, 0, 5, 6, 7, 8],
+        [1, 2, 3, 4, 5, 0, 6, 7, 8],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        [1, 0, 2, 3, 4, 5, 6, 7, 8],
+        [1, 2, 0, 3, 4, 5, 6, 7, 8],
+        [1, 2, 3, 0, 4, 5, 6, 7, 8],
+        [1, 2, 3, 4, 0, 5, 6, 7, 8],
+        [1, 2, 3, 4, 5, 0, 6, 7, 8],
+      ],
+      solved: false,
+    };
+  },
+  methods: {
+    changeIsStar() {
+      this.isStar = this.searchMethod == "1" ? true : false;
+    },
+    setOrder() {
+      const validateUserValue = this.userValue.trim().split("");
+      if (
+        validateUserValue.length == 9 &&
+        !isNaN(this.userValue) &&
+        validateUserValue.includes("0")
+      ) {
+        this.order = validateUserValue;
+        this.error = false;
+        this.success = true;
+      } else {
+        this.error = true;
+        this.success = false;
+      }
+    },
+    randomize() {
+      let newOrder = [];
+      while (newOrder.length != 9) {
+        let pos = Math.ceil(Math.random() * (9 - 0) - 1);
+
+        if (!newOrder.includes(pos)) {
+          newOrder.push(pos);
+        }
+      }
+      this.order = newOrder;
+    },
+    solve() {
+      if (this.searchMethod == 1) {
+        //logica da A* aqui
+      } else {
+        // logica da Largura aqui
+      }
+      this.solved = true;
+    },
+  },
+  watch: {
+    searchMethod() {
+      this.changeIsStar();
+    },
+  },
 });
 </script>
 
 <style scoped>
+.error {
+  border: 2px solid #ed5555;
+  color: #ed5555;
+}
+.success {
+  border: 2px solid #157347;
+  color: #157347;
+}
 .puzzle-temp {
   border-radius: 10px;
   display: grid;
@@ -72,10 +172,16 @@ export default defineComponent({
   grid-gap: 10px;
   padding: 10px;
   background-color: #232323;
-  max-height: 500px;
-  max-width: 500px;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  max-height: 450px;
+  max-width: 450px;
 }
 .puzzle-obj {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background: #333333;
   border-radius: 10px;
   font-size: 7rem;
@@ -85,5 +191,10 @@ export default defineComponent({
 }
 .puzzle-active {
   background: #157347;
+}
+
+.aside {
+  background: #232323;
+  border-radius: 10px;
 }
 </style>
